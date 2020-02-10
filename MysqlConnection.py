634cbd28c38e41991ca5847ff91007c1dbe64e10
adnantushar <44477872+adnantushar@users.noreply.github.com>
2020-02-10 23:48:50 +0600

    MySqlConnection

--- MysqlConnection.py
+++ MysqlConnection.py
@@ -1,9 +1,5 @@
 import mysql.connector
 
-print("hello ubuntu")
-for i in range(10):
-    print(i)
-
 db = mysql.connector.connect(host='localhost',user='root',password='0000',port='3306')
 
 cursor= db.cursor()
